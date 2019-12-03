0b63c4d85216804126a1906055c51dcf2ec2f7b9
Toppsdown <puzarne.e@gmail.com>
2019-12-03 10:20:53 -0500

    add control row interface

--- src/components/ControlRow.js
+++ src/components/ControlRow.js
@@ -3,10 +3,24 @@ import React from 'react';
 class ControlRow extends React.Component {
     render(){
         return (
-            <div className='row border' style={{height: '100px'}}>
-                <div className="col-sm" style={{}}></div>
-                <div className="col-sm-3" style={{}}></div>
-                <div className="col-sm" style={{}}></div>
+            <div className='row border' style={{}}>
+                <div className="col-sm" style={{}}>
+                    <div className='btn-group container' role='group' aria-label="Text Controls">
+                        <button className='btn btn-secondary'>▽</button>
+                        <button className='btn btn-secondary'>△</button>
+                    </div>
+                </div>
+                <div className="col-sm-3" style={{}}>
+                    <div className='btn-group container' role='group' aria-label="New Sync Area">
+                        <button className='btn btn-secondary'>+</button>
+                    </div>
+                </div>
+                <div className="col-sm" style={{}}>
+                    <div className='btn-group container' role='group' aria-label="Audio Controls">
+                        <button className='btn btn-secondary'>▽</button>
+                        <button className='btn btn-secondary'>△</button>
+                    </div>
+                </div>
             </div>
         );
     };
